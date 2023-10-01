b28f9ccd17d183533cd1677cfd8204e6adf35183
monjuaflame <monjuaflame@gmail.com>
2023-10-01 12:52:25 +0600

    add more shortcodes

--- shortcodes/shortcodes.php
+++ shortcodes/shortcodes.php
@@ -318,3 +318,259 @@ function comet_portfolio_section($attr, $content)
 }
 
 
+add_shortcode( 'home-vision', 'comet_home_vision' );
+
+function comet_home_vision($atts, $content = NULL){
+
+    $attrebut = extract(shortcode_atts( array(
+
+      'title'            => 'The Vision',
+      'subtitle'         => 'Not just code.',
+      'right_img'           => '',
+
+       'vision_one_title'      => 'Strategy', 
+       'vision_one_paragraph'  => 'Natus totam adipisci illum aut nihil consequuntur ut, ducimus alias iusto facili.',
+
+       'vision_two_title'      => 'Design', 
+       'vision_two_paragraph'  => 'Nisi, ut commodi dolor, quae doloremque earum alias accusantium sint.',
+
+       'vision_three_title'    => 'Skills', 
+       'vision_three_paragraph' => 'Nesciunt est eaque, expedita cum minima reprehenderit? Harum vero dolorum.', 
+
+       'vision_four_title'     => 'Power', 
+       'vision_four_paragraph' => 'Fuga ipsum, repellendus? Architecto commodi magni non nihil et iusto.',
+
+
+    ), $atts));
+
+	ob_start();
+
+    $imageSrc = wp_get_attachment_image_src($right_img, 'full'); 
+?>
+
+    <section>
+      <div class="col-md-6 col-sm-4 img-side img-right">
+        <div class="img-holder"><img src="<?php echo $imageSrc[0]; ?>" alt="" class="bg-img"></div>
+      </div>
+      <div class="container">
+        <div class="col-md-5 col-sm-8">
+          <div class="title">
+            <h4 class="upper"><?php echo $subtitle; ?></h4>
+            <h3><?php echo $title; ?><span class="red-dot"></span></h3>
+            <hr>
+          </div>
+          <div class="col-md-6 col-sm-6">
+            <div class="row">
+              <div class="text-box">
+                <h4 class="upper small-heading"><?php echo $vision_one_title; ?></h4>
+                <p><?php echo $vision_one_paragraph; ?></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-sm-6">
+            <div class="row">
+              <div class="text-box">
+                <h4 class="upper small-heading"><?php echo $vision_two_title; ?></h4>
+                <p><?php echo $vision_two_paragraph; ?></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-sm-6">
+            <div class="row">
+              <div class="text-box">
+                <h4 class="upper small-heading"><?php echo $vision_three_title; ?></h4>
+                <p><?php echo $vision_three_paragraph; ?></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-sm-6">
+            <div class="row">
+              <div class="text-box">
+                <h4 class="upper small-heading"><?php echo $vision_four_title; ?></h4>
+                <p><?php echo $vision_four_paragraph; ?></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+<?php 
+   return ob_get_clean();
+}
+
+
+
+add_shortcode( 'home-testimonials', 'comet_home_testimonials' );
+function comet_home_testimonials($atts, $content = NULL){
+	$attrebut = extract(shortcode_atts( array(
+
+      'title'            => 'What They Say',
+      'bg_img'         => '',
+
+    ), $atts));
+
+	ob_start();
+    $testimonialsimg = wp_get_attachment_image_src($bg_img, 'full');
+?>
+    <section class="parallax">
+      <div data-parallax="scroll" data-image-src="<?php echo $testimonialsimg[0]; ?>" class="parallax-bg"></div>
+      <div class="parallax-overlay pb-50 pt-50">
+        <div class="container">
+          <div class="title center">
+            <h3><?php echo $title; ?><span class="red-dot"></span></h3>
+            <hr>
+          </div>
+          <div class="section-content">
+            <div id="testimonials-slider" data-options="{&quot;animation&quot;: &quot;slide&quot;, &quot;controlNav&quot;: true}" class="flexslider nav-outside">
+              <ul class="slides">
+                <?php 
+          				$home_testimonials = new WP_Query(array(
+          					'post_type' => 'home-testimonials'
+          				));
+        			 ?>
+        			  <?php while($home_testimonials->have_posts()) : $home_testimonials->the_post();?>
+    	           <li>
+                    <blockquote>
+                      <p>"<?php echo the_content(); ?>"</p>
+                      <footer><?php echo the_title(); ?></footer>
+                    </blockquote>
+                  </li>
+                <?php endwhile; ?>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+<?php 
+   return ob_get_clean();
+}
+
+
+add_shortcode( 'home-clients', 'comet_home_clients' );
+
+function comet_home_clients($atts, $content = NULL){
+
+	$attrebut = extract(shortcode_atts( array(
+
+      'title'            => 'Our Clients',
+      'subtitle'         => 'Some of the best.',
+
+      'client_one'       => '',
+      'client_two'       => '',
+      'client_three'       => '',
+      'client_four'       => '',
+      'client_five'       => '',
+      'client_six'       => '',
+
+    ), $atts));
+
+	ob_start();
+
+$client_one_src = wp_get_attachment_image_src($client_one, 'full'); 
+$client_two_src = wp_get_attachment_image_src($client_two, 'full'); 
+$client_three_src = wp_get_attachment_image_src($client_three, 'full'); 
+$client_four_src = wp_get_attachment_image_src($client_four, 'full'); 
+$client_five_src = wp_get_attachment_image_src($client_five, 'full'); 
+$client_six_src = wp_get_attachment_image_src($client_six, 'full'); 
+?>
+    <section>
+      <div class="container">
+        <div class="title center">
+          <h4 class="upper"><?php echo $subtitle ?></h4>
+          <h3><?php echo $title ?><span class="red-dot"></span></h3>
+          <hr>
+        </div>
+        <div class="section-content">
+          <div class="boxes clients">
+            <div class="col-sm-4 col-xs-6 border-right border-bottom">
+              <img src="<?php  echo $client_one_src[0];  ?>" alt="" data-animated="true" class="client-image">
+            </div>
+            <div class="col-sm-4 col-xs-6 border-right border-bottom">
+              <img src="<?php  echo $client_two_src[0];  ?>" alt="" data-animated="true" data-delay="500" class="client-image">
+            </div>
+            <div class="col-sm-4 col-xs-6 border-bottom">
+              <img src="<?php  echo $client_three_src[0];  ?>" alt="" data-animated="true" data-delay="1000" class="client-image">
+            </div>
+            <div class="col-sm-4 col-xs-6 border-right">
+              <img src="<?php  echo $client_four_src[0];  ?>" alt="" data-animated="true" class="client-image">
+            </div>
+            <div class="col-sm-4 col-xs-6 border-right">
+              <img src="<?php  echo $client_five_src[0];  ?>" alt="" data-animated="true" data-delay="500" class="client-image">
+            </div>
+            <div class="col-sm-4 col-xs-6">
+              <img src="<?php  echo $client_six_src[0];  ?>" alt="" data-animated="true" data-delay="1000" class="client-image">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+<?php 
+   return ob_get_clean();
+}
+
+
+add_shortcode( 'home-blog', 'comet_home_blog' );
+
+function comet_home_blog($atts, $content = NULL){
+
+    $attrebut = extract(shortcode_atts( array(
+
+      'title'            => 'The Blog',
+      'subtitle'         => 'We have a few tips for you.',
+      'blog_btn_link'    => '',
+      'post_per_page'    => 2
+
+
+    ), $atts));
+
+	ob_start();
+?>
+
+
+   <section>
+      <div class="container">
+        <div class="title center">
+          <h4 class="upper"><?php echo $subtitle ?></h4>
+          <h2><?php echo $title ?><span class="red-dot"></span></h2>
+          <hr>
+        </div>
+        <div class="section-content">
+          <div class="col-md-8 col-md-offset-2">
+            <?php 
+
+
+
+            	$home_blog = new WP_Query(array(
+            		'post_type' => 'post',
+                'posts_per_page' => $post_per_page,
+            	));
+            ?>
+
+        	<?php while($home_blog->have_posts()) : $home_blog->the_post();?>
+                <div class="blog-post">
+                  <div class="post-body">
+                    <h3 class="serif"><a href="<?php echo the_permalink(); ?>"><?php echo the_title(); ?></a></h3>
+                    <hr>
+                    <p class="serif"><?php echo wp_trim_words( get_the_content(), 40, '...' ); ?></p>
+                    <div class="post-info upper"><a href="<?php echo the_permalink(); ?>">Read More</a><span class="pull-right black-text"><?php the_time('F j, Y'); ?></span></div>
+                  </div>
+                </div>
+            <?php endwhile; ?>
+            
+          </div>
+          <div class="clearfix"></div>
+          <div class="mt-25">
+            <p class="text-center"><a href="<?php echo $blog_btn_link; ?>" class="btn btn-color-out">View Full Blog</a></p>
+          </div>
+        </div>
+      </div>
+    </section>
+     
+<?php 
+   return ob_get_clean();
+}
